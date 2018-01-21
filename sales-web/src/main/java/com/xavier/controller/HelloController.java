d4ba17cd8a651f6a935117a54400bd36b404c5f3
xavier <xavier@zju.edu.cn>
2018-01-21 21:20:42 +0800

    Add service module and thymeleaf

--- sales-web/src/main/java/com/xavier/controller/HelloController.java
+++ sales-web/src/main/java/com/xavier/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.xavier.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by xavier on 2018/1/21.
  */
-@RestController
+@Controller
 public class HelloController {
 
 	@RequestMapping("hello")
-	public String hello() {
-		return "Hello World!";
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+	                    ModelMap modelMap) {
+		modelMap.addAttribute("name", name);
+		return "hello";
+	}
+
+	@RequestMapping("/")
+	public String toIndex() {
+		return "index";
 	}
 }
